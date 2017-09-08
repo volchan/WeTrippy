96331181f53f0304c821560c11d00145fa853b12
stephane delatorre <s.dlt@hotmail.fr>
2017-09-08 14:20:32 +0200

    fixed rating display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def display_rating(rating)
-    if rating.zero?
+    if rating < 0.25
       image_tag('empty_star.svg', class: 'rating-star').to_s * 5 + " #{rating}"
     elsif rating >= 0.25 && rating < 0.5
       "#{image_tag('1_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 4}" + " #{rating}"
@@ -8,7 +8,7 @@ module ApplicationHelper
       "#{image_tag('half_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 4}" + " #{rating}"
     elsif rating >= 0.75 && rating < 1
       "#{image_tag('3_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 4}" + " #{rating}"
-    elsif rating == 1
+    elsif rating >= 1 && rating < 1.25
       "#{image_tag('full_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 4}" + " #{rating}"
     elsif rating >= 1.25 && rating < 1.5
       "#{image_tag('full_star.svg', class: 'rating-star')}#{image_tag('1_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 3}" + " #{rating}"
@@ -16,7 +16,7 @@ module ApplicationHelper
       "#{image_tag('full_star.svg', class: 'rating-star')}#{image_tag('half_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 3}" + " #{rating}"
     elsif rating >= 1.75 && rating < 2
       "#{image_tag('full_star.svg', class: 'rating-star')}#{image_tag('3_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 3}" + " #{rating}"
-    elsif rating == 2
+    elsif rating >= 2 && rating < 2.25
       "#{image_tag('full_star.svg', class: 'rating-star') * 2}#{image_tag('empty_star.svg', class: 'rating-star') * 3}"
     elsif rating >= 2.25 && rating < 2.5
       "#{image_tag('full_star.svg', class: 'rating-star') * 2}#{image_tag('1_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 2}" + " #{rating}"
@@ -24,7 +24,7 @@ module ApplicationHelper
       "#{image_tag('full_star.svg', class: 'rating-star') * 2}#{image_tag('half_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 2}" + " #{rating}"
     elsif rating >= 2.75 && rating < 3
       "#{image_tag('full_star.svg', class: 'rating-star') * 2}#{image_tag('3_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 2}" + " #{rating}"
-    elsif rating == 3
+    elsif rating >= 3 && rating < 3.25
       "#{image_tag('full_star.svg', class: 'rating-star') * 3}#{image_tag('empty_star.svg', class: 'rating-star') * 2}"
     elsif rating >= 3.25 && rating < 3.5
       "#{image_tag('full_star.svg', class: 'rating-star') * 3}#{image_tag('1_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 1}" + " #{rating}"
@@ -32,7 +32,7 @@ module ApplicationHelper
       "#{image_tag('full_star.svg', class: 'rating-star') * 3}#{image_tag('half_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 1}" + " #{rating}"
     elsif rating >= 3.75 && rating < 4
       "#{image_tag('full_star.svg', class: 'rating-star') * 3}#{image_tag('3_4_star.svg', class: 'rating-star')}#{image_tag('empty_star.svg', class: 'rating-star') * 1}" + " #{rating}"
-    elsif rating == 4
+    elsif rating >= 4 && rating < 4.25
       "#{image_tag('full_star.svg', class: 'rating-star') * 4}#{image_tag('empty_star.svg', class: 'rating-star') * 2}" + " #{rating}"
     elsif rating >= 4.25 && rating < 4.5
       "#{image_tag('full_star.svg', class: 'rating-star') * 4}#{image_tag('1_4_star.svg', class: 'rating-star')}" + " #{rating}"
